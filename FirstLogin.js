5d28945b20d79a71568ade5d434d1db5d7bf7c1b
rohitdeshpande387 <68802021+rohitdeshpande387@users.noreply.github.com>
2020-07-29 12:39:22 +0530

    Add files via upload

---
+++ FirstLogin.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("userform");
+const loginButton = document.getElementById("submitdetails");
+const loginErrorMsg = document.getElementById("errormessage");
+const loginwhat = document.getElementById("what");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "rohit" && password === "web_dev") {
+
+      loginwhat.style.opacity = 1;
+        
+        
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
